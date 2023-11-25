63c46f92669741a52543be4f524929bd16234579
zhj <z1871434921qq@hotmail.com>
2023-11-25 17:29:13 +0800

    设置Bottom Navigation中的MenuItem，并绑定指定的Fragment

--- app/src/main/java/com/example/exam/MainActivity2.java
+++ app/src/main/java/com/example/exam/MainActivity2.java
@@ -23,15 +23,15 @@ public class MainActivity2 extends AppCompatActivity {
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView navView = (BottomNavigationView) findViewById(R.id.nav_view);
+        navView.setItemIconTintList(null);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_task, R.id.navigation_award, R.id.navigation_statistics, R.id.navigation_my)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
